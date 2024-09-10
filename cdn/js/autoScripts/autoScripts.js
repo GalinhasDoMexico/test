02b2ade63a8a88df1286c697c8da0b6121585586
GalinhasDoMexico <47695682+GalinhasDoMexico@users.noreply.github.com>
2024-09-10 18:32:44 -0300

    Update autoScripts.js

--- cdn/js/autoScripts/autoScripts.js
+++ cdn/js/autoScripts/autoScripts.js
@@ -14,43 +14,43 @@ function addPageSpecificScripts() {
   var lang = document.documentElement.lang; // Obtém o idioma da página
 
   // Adiciona scripts comuns
-  addScript('/cdn/js/mobile-menu.js', 'defer');
-  addScript('/cdn/js/autoNavAndFooter.js', 'defer');
-  addScript('/cdn/js/menuHref.js', 'defer');
-  addScript('/cdn/js/theme-switcher.js', 'defer');
-  addScript('/cdn/js/choose-language.js', 'defer');
-  addScript('/cdn/js/lgbt-mode.js', 'defer');
-  addScript('/cdn/js/updateVersionText.js', 'defer');
+  addScript('/test/cdn/js/mobile-menu.js', 'defer');
+  addScript('/test/cdn/js/autoNavAndFooter.js', 'defer');
+  addScript('/test/cdn/js/menuHref.js', 'defer');
+  addScript('/test/cdn/js/theme-switcher.js', 'defer');
+  addScript('/test/cdn/js/choose-language.js', 'defer');
+  addScript('/test/cdn/js/lgbt-mode.js', 'defer');
+  addScript('/test/cdn/js/updateVersionText.js', 'defer');
 
   // Adiciona scripts específicos para a página /blog/
-  if (path === '/blog/') {
-      addScript('/cdn/js/blog/search/blogSearchScript.js', 'defer');
-      addScript('/cdn/js/blog/blogAlerts.js', 'defer');
+  if (path === '/test/blog/') {
+      addScript('/test/cdn/js/blog/search/blogSearchScript.js', 'defer');
+      addScript('/test/cdn/js/blog/blogAlerts.js', 'defer');
   }
 
   // Adiciona scripts específicos para a página /about/
-  if (path === '/br/about-me/' || path === '/us/about-me/') {
+  if (path === '/test/br/about-me/' || path === '/test/us/about-me/') {
       addScript('https://www.youtube.com/iframe_api', 'defer');
-      addScript('/cdn/js/playlist-player.js', 'defer');
+      addScript('/test/cdn/js/playlist-player.js', 'defer');
   }
 
   // Adiciona scripts específicos para a página /portfolio/design-2d
-  if (path === '/br/portfolio/design-2d' || path === '/us/portfolio/design-2d') {
+  if (path === '/test/br/portfolio/design-2d' || path === '/test/us/portfolio/design-2d') {
       addScript('/cdn/js/portfolio/portfolioImageFullscreen.js', 'defer');
       if (lang === 'pt-br') {
-          addScript('/cdn/js/portfolio/portfolio2DDesignSearchScript.js', 'defer');
+          addScript('/test/cdn/js/portfolio/portfolio2DDesignSearchScript.js', 'defer');
       } else if (lang === 'en-us') {
-          addScript('/cdn/js/portfolio/us/portfolioUs2DDesignSearchScript.js', 'defer');
+          addScript('/test/cdn/js/portfolio/us/portfolioUs2DDesignSearchScript.js', 'defer');
       }
   }
 
   // Adiciona scripts específicos para a página /portfolio/minecraft
-  if (path === '/br/portfolio/minecraft' || path === '/us/portfolio/minecraft') {
-      addScript('/cdn/js/portfolio/portfolioImageFullscreen.js', 'defer');
+  if (path === '/test/br/portfolio/minecraft' || path === '/test/us/portfolio/minecraft') {
+      addScript('/test/cdn/js/portfolio/portfolioImageFullscreen.js', 'defer');
       if (lang === 'pt-br') {
-          addScript('/cdn/js/portfolio/portfolioMinecraftSearchScript.js', 'defer');
+          addScript('/test/cdn/js/portfolio/portfolioMinecraftSearchScript.js', 'defer');
       } else if (lang === 'en-us') {
-          addScript('/cdn/js/portfolio/portfolioUsMinecraftScript.js', 'defer');
+          addScript('/test/cdn/js/portfolio/portfolioUsMinecraftScript.js', 'defer');
       }
   }
 
